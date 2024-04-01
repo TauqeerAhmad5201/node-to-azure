a5916a3a6f1768cb72ae93d05009853e4966f8c0
tauqeerahmad5201 <hellotauqeer@gmail.com>
2024-04-02 05:06:31 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
